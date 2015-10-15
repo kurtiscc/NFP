afdf01c58fec53e9ecf00a3ce18a96d383a46a22
Kurtis Christensen <kurtischadchristensen@gmail.com>
2015-10-15 14:59:06 -0600

    added timestamp, latitude, longitude

---
+++ app/src/main/java/it446/nfp/MainActivity.java
@@ -0,0 +1,124 @@
+package it446.nfp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    public static Double lat;
+    public static Double lng;
+    private String latitude, longitude;
+    TextView testTimeStamp;
+    TextView testLat;
+    TextView testLng;
+
+    /**
+     *
+     * @return yyyy-MM-dd HH:mm:ss formate date as string
+     */
+    public static String getCurrentTimeStamp(){
+        try {
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTimeStamp = dateFormat.format(new Date()); // Find todays date
+
+            return currentTimeStamp;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            updateWithNewLocation(location);
+        }
+
+        public void onProviderDisabled(String provider) {}
+        public void onProviderEnabled(String provider) {}
+        public void onStatusChanged(String provider, int status,
+                                    Bundle extras) {}
+    };
+
+    private void updateWithNewLocation(Location location) {
+
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testTimeStamp = (TextView) findViewById(R.id.testTimeStamp);
+        testLat = (TextView) findViewById(R.id.testLat);
+        testLng = (TextView) findViewById(R.id.testLng);
+
+        LocationManager locationManager;
+        String svcName = Context.LOCATION_SERVICE;
+        locationManager = (LocationManager)getSystemService(svcName);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+
+        String provider = locationManager.getBestProvider(criteria, true);
+
+                Location l = locationManager.getLastKnownLocation(provider);
+
+                updateWithNewLocation(l);
+
+                locationManager.requestLocationUpdates(provider, 2000, 10,
+                        locationListener);
+
+        testTimeStamp.setText(getCurrentTimeStamp());
+        testLat.setText(String.valueOf(lat));
+        testLng.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
