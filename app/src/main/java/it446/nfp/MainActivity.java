a8b2401e1d96a8f951d62c0c6b5dfd2b784bbdca
Kurtis Christensen <kurtischadchristensen@gmail.com>
2015-10-21 19:10:51 -0600

    Fixed API 23 error for location check

--- app/src/main/java/it446/nfp/MainActivity.java
+++ app/src/main/java/it446/nfp/MainActivity.java
@@ -9,13 +9,24 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +35,6 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     public static Double lat;
     public static Double lng;
-    private String latitude, longitude;
     TextView testTimeStamp;
     TextView testLat;
     TextView testLng;
@@ -86,14 +96,32 @@ public class MainActivity extends AppCompatActivity {
         criteria.setSpeedRequired(false);
         criteria.setCostAllowed(true);
 
-        String provider = locationManager.getBestProvider(criteria, true);
+        if (Build.VERSION.SDK_INT == 23) {
 
-                Location l = locationManager.getLastKnownLocation(provider);
+            if (locationManager != null) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        || checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    String provider = locationManager.getBestProvider(criteria, true);
 
-                updateWithNewLocation(l);
+                    Location l = locationManager.getLastKnownLocation(provider);
 
-                locationManager.requestLocationUpdates(provider, 2000, 10,
-                        locationListener);
+                    updateWithNewLocation(l);
+
+                    locationManager.requestLocationUpdates(provider, 2000, 10,
+                            locationListener);
+                }
+            }
+
+        } else {
+            String provider = locationManager.getBestProvider(criteria, true);
+
+            Location l = locationManager.getLastKnownLocation(provider);
+
+            updateWithNewLocation(l);
+
+            locationManager.requestLocationUpdates(provider, 2000, 10,
+                    locationListener);
+        }
 
         testTimeStamp.setText(getCurrentTimeStamp());
         testLat.setText(String.valueOf(lat));
@@ -120,5 +148,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public class PostJSONData extends AsyncTask<Void, Void, Boolean> {
+
+        public PostJSONData() {}
+        JSONObject jsonObject = null;
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            String url = "http://url.com";
+            try {
+
+                URL object = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) object.openConnection();
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                con.setRequestProperty("Accept", "application/json");
+                con.setRequestMethod("POST");
+
+
+                OutputStream os = con.getOutputStream();
+                os.write(jsonObject.toString().getBytes("UTF-8"));
+                os.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return false;
+        }
     }
 }
