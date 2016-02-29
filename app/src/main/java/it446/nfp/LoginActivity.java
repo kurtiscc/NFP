273a0b82aeee906cdd009a46c020b0be892ade3d
Kurtis Christensen <kurtischadchristensen@gmail.com>
2016-02-28 21:27:49 -0700

    deleting master

--- app/src/main/java/it446/nfp/LoginActivity.java
+++
@@ -1,7 +0,0 @@
-package it446.nfp;
-
-/**
- * Created by ChristensenKC on 10/7/2015.
- */
-public class LoginActivity {
-}
--- app/src/main/java/it446/nfp/MainPatientActivity.java
+++
@@ -1,7 +0,0 @@
-package it446.nfp;
-
-/**
- * Created by ChristensenKC on 11/16/2015.
- */
-public class MainPatientActivity {
-}
