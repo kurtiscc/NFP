b13aa22a856404c9778f284334e940e43bea0ddf
Alex Mills <alex.mills70@gmail.com>
2016-03-11 14:00:17 -0700

    changing the connection string

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var MC = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var async = require('async');
 
-var MongoConnectionString = "mongodb://nfpuser:nfpAPPuser@40.78.57.211:27017/NFP";
+//var MongoConnectionString = "mongodb://nfpuser:nfpAPPuser@40.78.57.211:27017/NFP";
 //MongoConnectionString
+var MongoConnectionString = "mongodb://localhost:27017/NFP";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
