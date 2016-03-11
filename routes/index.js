fd05e7e85a7c3312099127facf9a4d9ba72c6b25
ldimans <ldimans@users.noreply.github.com>
2016-03-10 22:17:49 -0700

    Update index.js
    trying connection string

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var MC = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var async = require('async');
 
-var uri = "mongodb://nfpuser:nfpAPPuser@40.78.57.211:27017/NFP";
-MongoConnectionString
+var MongoConnectionString = "mongodb://nfpuser:nfpAPPuser@40.78.57.211:27017/NFP";
+//MongoConnectionString
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
